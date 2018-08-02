9f83e02763bdb488907e9b115064cdb493226bea
Daniel Abeles <dabeles@akamai.com>
2018-08-02 14:50:32 +0300

    updated the startup script

--- start_docker_cli.sh
+++ start_docker_cli.sh
@@ -1,1 +1,10 @@
+echo "running mqtt-pwn cli..."
+
+if [[ $(docker-compose ps | grep "mqtt-pwn_db_1") ]]; then
+    echo "db exists, skipping creation..."
+else
+    echo "db not exists, creating it..."
+    docker-compose up --build --detach
+fi
+
 docker-compose run --rm cli
